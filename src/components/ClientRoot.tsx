e0779b3b6deac3ee5780da3d2f0162b7ef2b7bc0
theGeorge123 <maxmeinders2002@gmail.com>
2025-07-07 23:30:18 +0200

    fix i18n init

--- src/components/ClientRoot.tsx
+++ src/components/ClientRoot.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Providers } from "@/components/providers";
+import '@/i18n';
 
 export default function ClientRoot({ children }: { children: React.ReactNode }) {
   return <Providers>{children}</Providers>;
